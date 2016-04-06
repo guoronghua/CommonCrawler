5a68fe0cf3db1b8bf0392b218f3ed2caa0b59c9e
guoronghua <373473032@qq.com>
2016-04-06 09:33:44 +0800

    4.6

--- app/auth/views.py
+++ app/auth/views.py
@@ -33,10 +33,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
-            return render_template('auth/dsfdn.html', form=form)
-
-        #     return redirect(request.args.get('next') or url_for('main.Index'))
-        return render_template('auth/dsfdn.html', form=form)
+            return redirect(request.args.get('next') or url_for('main.Index'))
+        return render_template('auth/unconfirmed.html', form=form)
     return render_template('auth/login.html', form=form)
 
 
